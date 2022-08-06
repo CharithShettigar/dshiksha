7e2aa440554ff3236db2a429a809fb5df32ef236
CharithShettigar <charithshettigar3964@gmail.com>
2022-08-06 12:18:27 +0530

    after merge conflict

--- dshiksha/settings.py
+++ dshiksha/settings.py
@@ -97,9 +97,9 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dshiksha',
-        'USER': 'root',
-        'PASSWORD': '12345678',
+        'NAME': 'dshikshadbtest',
+        'USER': 'Others',
+        'PASSWORD': '12345',
         'HOST': 'localhost',
     }
 }
